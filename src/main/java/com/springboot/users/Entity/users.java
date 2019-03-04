95304d3193f680978086ae617a09f61675f039b0
lsamvura <lsamvura@psmi.co.zw>
2019-03-04 13:33:41 +0200

    seccond commit

--- src/main/java/com/springboot/users/Entity/users.java
+++ src/main/java/com/springboot/users/Entity/users.java
@@ -1,5 +1,6 @@
 package com.springboot.users.Entity;
 
+import com.springboot.init.BaseEntity;
 import lombok.Data;
 
 import javax.persistence.Entity;
@@ -8,7 +9,7 @@ import javax.persistence.Table;
 @Entity
 @Data
 @Table(name = "users")
-public class users {
+public class users extends BaseEntity {
     private String userId;
     private String userName;
     private String gender;
