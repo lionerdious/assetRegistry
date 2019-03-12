1f14195f6f620af4f2bb0029ad741f03d25813ba
lsamvura <lsamvura@psmi.co.zw>
2019-03-12 14:48:19 +0200

    Asset type

--- src/main/java/com/springboot/asset/type/controller/AssetTypeController.java
+++ src/main/java/com/springboot/asset/type/controller/AssetTypeController.java
@@ -2,6 +2,7 @@ package com.springboot.asset.type.controller;
 
 import com.springboot.asset.type.entity.AssetType;
 import com.springboot.asset.type.service.AssetTypeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.util.List;
 
 @Controller
+@Slf4j
 public class AssetTypeController {
 
     @Autowired
